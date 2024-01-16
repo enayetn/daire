37fdbbbef03c75ba9fb61d16ef954990fdfda132
nabil <nenayet@anduril.com>
2024-01-15 21:47:52 -0500

    Initial commit of daire, generate .md files with today's date

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::env;
+use chrono::Local;
+
+fn main() {
+    let mut file = Local::now()
+        .format("%b%d")
+        .to_string()
+        .to_uppercase();
+    let note_template = format!("#{}
+
+Brain dump
+- 
+    ", file);
+    file.push_str(".md");
+    let cwd = env::current_dir()
+        .expect("ERROR")
+        .join(file);
+    fs::write(cwd.as_os_str(), note_template)
+        .expect("Error in writing file");
+}
